6c8e01db27e96b546c8533094fa2ca36c007eb74
MarCnu <marceau.cnudde@gmail.com>
2014-07-23 11:06:04 +0200

    Remove JS requirement

--- pdf/pdf.py
+++ pdf/pdf.py
@@ -79,7 +79,6 @@ class pdfXBlock(XBlock):
         
         frag = Fragment(html)
         frag.add_css(self.load_resource("static/css/pdf.css"))
-        frag.add_javascript(self.load_resource("static/js/pdf.js"))
         frag.add_javascript(self.load_resource("static/js/pdf_view.js"))
         frag.initialize_js('pdfXBlockInitView')
         return frag
