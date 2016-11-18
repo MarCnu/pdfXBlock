b59e1ec7a44bd4851d7e3f3b2dd41c0c3e6a6e7c
Ivan Kovic <kovic.ivan@gmail.com>
2016-11-18 11:26:52 +0100

    Added forgotten fields

--- pdf/static/js/pdf_edit.js
+++ pdf/static/js/pdf_edit.js
@@ -13,6 +13,11 @@ function pdfXBlockInitEdit(runtime, element) {
         data.append('usage_id', $element.data('usage-id'));
         data.append('display_name', $element.find('#pdf_edit_display_name').val());
         data.append('display_description', $element.find('pdf_edit_display_description').val());
+        data.append('source_text', $element.find('#pdf_edit_source_text').val());
+        data.append('source_url', $element.find('#pdf_edit_source_url').val());
+        data.append('url', $element.find('#pdf_edit_url').val());
+        data.append('allow_download', $element.find('#pdf_edit_allow_download').val());
+        data.append('source_url', $element.find('#pdf_edit_source_url').val());
         data.append('thumbnail', $element.find('input[name=thumbnail]')[0].files[0]);
 
         runtime.notify('save', {state: 'start'});
